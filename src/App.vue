9e150c24e6d07c6b106f85f9ed0f6db59a7a3113
architgoyal07 <architcool09@gmail.com>
2020-01-15 02:17:58 +0530

    Home Page Basic

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
+
+      <home/>
+
     <router-view />
   </div>
 </template>
 
+<script>
+
+  import Home from './components/Home'
+
+export default {
+  name:'app',
+  components:{
+    Home
+  }
+}
+</script>
+
 <style lang="scss">
+
+*{
+  box-sizing: border-box;
+  margin:0px;
+  padding: 0px;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
+  background-color: black;
 }
 
-#nav {
-  padding: 30px;
+/* #nav {
+   padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+   a {
+     font-weight: bold;
+     color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+     &.router-link-exact-active {
+       color: #42b983;
+     }
+   }
+ }*/
+
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home" >
+    
+    <div class="image">
+      <img src="../assets/archit2.jpg" width="30%" height="auto" >
+    </div>
+
+    <div class="title">
+      <h1>Archit Goyal</h1>
+      <br>
+      <h4>FrontEnd Developer</h4>
+    </div>
+<br>
+    <div class="links">
+        
+         <a href="https://www.linkedin.com/in/archit-goyal-8a6957133/"><span class="mdi mdi-linkedin"></span></a>
+         <a href="https://app.slack.com/client/TDGG421C1/DR80DRPJA/user_profile/WLLJMQLPL"><span class="mdi mdi-slack"></span></a>
+         <a href="https://github.com/architgoyal07"><span class="mdi mdi-github-circle"></span></a>
+         <a href="https://twitter.com/i_Am_archit"> <span class="mdi mdi-twitter"></span></a>
+         <a href="https://www.facebook.com/archit.goyal.77"> <span class="mdi mdi-facebook"></span></a>
+         <a href="https://www.instagram.com/_archit_goyal/"> <span class="mdi mdi-instagram"></span></a>
+          
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+};
+</script>
+
+
+<style scoped lang="scss">
+
+ 
+
+  .home{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
+  h1{
+    font-size: 5vw;
+    font-family: sans-serif;
+  }
+
+  h4{
+    color: lightgray;
+    font-size: 2vw;
+  }
+  .links{
+    font-size: 2vw;
+  }
+
+  a{
+    color: lightslategray;
+  }
+  a:hover{
+    color: white;
+  }
+</style>
