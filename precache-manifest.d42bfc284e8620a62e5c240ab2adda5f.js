f7fa44b98ba2332e76f96a9d5d216b84eeb1bef8
architgoyal07 <architcool09@gmail.com>
2020-02-03 19:38:53 +0530

    Initial dist subtree commit

---
+++ precache-manifest.d42bfc284e8620a62e5c240ab2adda5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcea01b2e5feda1a1e7c",
+    "url": "<corporate_profile>/css/app.b9e9ebe1.css"
+  },
+  {
+    "revision": "28ba875d3c0102bde5f22e330e1605cd",
+    "url": "<corporate_profile>/img/archit2.28ba875d.jpg"
+  },
+  {
+    "revision": "bada2feae86f4421455c71b5a5d74b96",
+    "url": "<corporate_profile>/index.html"
+  },
+  {
+    "revision": "67896d1b3a91a20ceaaa",
+    "url": "<corporate_profile>/js/about.0d0ed103.js"
+  },
+  {
+    "revision": "dcea01b2e5feda1a1e7c",
+    "url": "<corporate_profile>/js/app.94a4b2bc.js"
+  },
+  {
+    "revision": "bb6ff6f32a92638006e5",
+    "url": "<corporate_profile>/js/chunk-vendors.4e99ef89.js"
+  },
+  {
+    "revision": "1e6801198604a69048dd54db659f7ad9",
+    "url": "<corporate_profile>/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "<corporate_profile>/robots.txt"
+  }
+]);
