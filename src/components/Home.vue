b694ecd9baa0c91703b95615d87c287ff67b31e8
Archit Goyal <33389185+architgoyal07@users.noreply.github.com>
2020-05-26 22:33:33 +0530

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,7 +18,7 @@
         
          <a href="https://www.linkedin.com/in/archit-goyal-8a6957133/"><span class="mdi mdi-linkedin"></span></a>
          <a href="https://app.slack.com/client/TDGG421C1/DR80DRPJA/user_profile/WLLJMQLPL"><span class="mdi mdi-slack"></span></a>
-         <a href="https://github.com/architgoyal07"><span class="mdi mdi-github-circle"></span></a>
+         <a href="https://github.com/architgoyal07"><span class="mdi mdi-github"></span></a>
          <a href="https://twitter.com/i_Am_archit"> <span class="mdi mdi-twitter"></span></a>
          <a href="https://www.facebook.com/archit.goyal.77"> <span class="mdi mdi-facebook"></span></a>
          <a href="https://www.instagram.com/_archit_goyal/"> <span class="mdi mdi-instagram"></span></a>
